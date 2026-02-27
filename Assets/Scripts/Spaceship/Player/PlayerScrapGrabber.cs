95a7885287162553da32197a332cb3c47a6711c5
Nugget <TheNuggetXYZ@gmail.com>
2026-02-27 17:16:33 +0100

    laser based high distance scrap attraction

--- Assets/Scripts/Spaceship/Player/PlayerScrapGrabber.cs
+++ Assets/Scripts/Spaceship/Player/PlayerScrapGrabber.cs
@@ -5,6 +5,68 @@ public class PlayerScrapGrabber : MonoBehaviour
 {
     private PlayerInputHandler input;
     
+    [SerializeField] private float grabRadius;
+    [SerializeField] private LayerMask scrapMask;
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private Transform grabPoint;
+    [SerializeField] private Transform targetPoint;
+    [SerializeField] private float attractForce;
+    [SerializeField] private float stabilizationForce;
+
+    private void Awake()
+    {
+        input = GetComponent<PlayerInputHandler>();
+    }
+
+    private void Update()
+    {
+        lineRenderer.positionCount = 0;
+        
+        if (input.isAttractingScrap)
+        {
+            Collider[] hits = GetParts();
+            DrawLasers(hits);
+            AttractParts(hits);
+        }
+    }
+
+    private Collider[] GetParts()
+    {
+        // cast a sphere ahead of the player
+        return Physics.OverlapSphere(grabPoint.position + transform.forward * grabRadius, grabRadius, scrapMask);
+    }
+
+    private void AttractParts(Collider[] hits)
+    {
+        foreach (Collider col in hits)
+        {
+            if (!col.attachedRigidbody)
+                continue;
+            
+            Vector3 toGrabPointDir = grabPoint.position - col.transform.position;
+            Vector3 toFrontOfPlayerDir = Vector3.ProjectOnPlane(targetPoint.position - col.transform.position, transform.forward);
+            float force = Mathf.Lerp(-attractForce, attractForce, Vector3.Distance(grabPoint.position, col.transform.position) / grabRadius);
+            //col.attachedRigidbody.AddForce(force * toGrabPointDir + stabilizationForce * toTargetPointDir.normalized);
+            col.attachedRigidbody.linearVelocity =
+                force * toGrabPointDir + stabilizationForce * toFrontOfPlayerDir.normalized;
+        }
+    }
+
+    private void DrawLasers(Collider[] hits)
+    {
+        lineRenderer.positionCount = hits.Length * 2 + 1;
+        for (int i = 0; i < hits.Length; i++)
+        {
+            lineRenderer.SetPosition(2*i, hits[i].transform.position);
+            lineRenderer.SetPosition(2*i + 1, grabPoint.position);
+        }
+    }
+}
+
+/*public class PlayerScrapGrabber : MonoBehaviour
+{
+    private PlayerInputHandler input;
+
     [SerializeField] private Rigidbody rb;
     [SerializeField] private Transform grabPoint;
     [SerializeField] private float grabRadius;
@@ -34,7 +96,7 @@ public class PlayerScrapGrabber : MonoBehaviour
 
             if (!hit.attachedRigidbody)
                 return;
-            
+
             if (distance <= stabilizeRadius)
             {
                 hit.attachedRigidbody.linearVelocity = rb.linearVelocity;
@@ -42,9 +104,10 @@ public class PlayerScrapGrabber : MonoBehaviour
             else
             {
                 float grabForceIntensity = Mathf.InverseLerp(0, grabRadius, distance);
-                
+
                 hit.attachedRigidbody.AddForce(inDirection * Mathf.Lerp(grabForceRange.x, grabForceRange.y, grabForceIntensity), ForceMode.Acceleration);
             }
         }
     }
 }
+*/
